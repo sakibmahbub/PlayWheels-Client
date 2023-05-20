2421355ab4b7e0bd9c9f51ba318f12079896af5e
sakibmahbub <arafatsakib48@gmail.com>
2023-05-20 21:55:29 +0600

    added new toy into database

--- src/Pages/AddAToy/AddAToy.jsx
+++ src/Pages/AddAToy/AddAToy.jsx
@@ -1,13 +1,61 @@
+import { useContext } from "react";
+import { AuthContext } from "../../Providers/AuthProvider";
+
 const AddAToy = () => {
+  const { user } = useContext(AuthContext);
+
+  const handleAddToy = (event) => {
+    event.preventDefault();
+
+    const form = event.target;
+    const picture = form.picture.value;
+    const name = form.name.value;
+    const seller = form.seller.value;
+    const sellerEmail = form.sellerEmail.value;
+    const subCategory = form.subCategory.value;
+    const price = form.price.value;
+    const rating = form.rating.value;
+    const quantity = form.quantity.value;
+    const description = form.description.value;
+
+    const toy = {
+      picture,
+      name,
+      price,
+      rating,
+      sub_category: subCategory,
+      seller,
+      sellerEmail,
+      quantity,
+      description,
+    };
+
+    console.log(toy);
+
+    fetch("http://localhost:5000/toys", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(toy),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        if (data.insertId) {
+          alert("toy added successfully");
+        }
+      });
+  };
   return (
     <div className="my-20">
       <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
         <h1 className="text-2xl font-bold leading-tight text-center tracking-tight text-gray-900 md:text-2xl mb-10 lg:text-3xl ">
           Add Your Desired Toy
         </h1>
-        <div className="w-full rounded-lg bg-yellow-50 p-5 md:mt-0 sm:max-w-md xl:p-0">
+        <div className="w-full rounded-lg bg-yellow-100 p-5 md:mt-0 sm:max-w-md xl:p-0">
           <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-            <form className="space-y-4 md:space-y-6 ">
+            <form onSubmit={handleAddToy} className="space-y-4 md:space-y-6 ">
               <div className="flex justify-center gap-4">
                 <div>
                   <label className="block mb-2 text-sm font-medium text-gray-900 ">
@@ -15,7 +63,7 @@ const AddAToy = () => {
                   </label>
                   <input
                     type="text"
-                    name="photo"
+                    name="picture"
                     className=" border border-gray-100 text-gray-700 sm:text-sm rounded-lg focus:ring-primary-400 focus:border-primary-400 block w-full p-2.5 "
                     required
                   />
@@ -40,7 +88,8 @@ const AddAToy = () => {
                   <input
                     type="text"
                     name="seller"
-                    className=" border border-gray-100 text-gray-700 sm:text-sm rounded-lg focus:ring-primary-400 focus:border-primary-400 block w-full p-2.5"
+                    defaultValue={user ? user.displayName : ""}
+                    className="border border-gray-100 text-gray-700 sm:text-sm rounded-lg focus:ring-primary-400 focus:border-primary-400 block w-full p-2.5"
                     required
                   />
                 </div>
@@ -50,7 +99,8 @@ const AddAToy = () => {
                   </label>
                   <input
                     type="text"
-                    name="seller-mail"
+                    name="sellerEmail"
+                    defaultValue={user ? user.displayName : ""}
                     className=" border border-gray-100 text-gray-700 sm:text-sm rounded-lg focus:ring-primary-400 focus:border-primary-400 block w-full p-2.5"
                     required
                   />
@@ -63,7 +113,7 @@ const AddAToy = () => {
                   </label>
                   <input
                     type="text"
-                    name="Sub-category"
+                    name="subCategory"
                     className=" border border-gray-100 text-gray-700 sm:text-sm rounded-lg focus:ring-primary-400 focus:border-primary-400 block w-full p-2.5"
                     required
                   />
@@ -112,14 +162,14 @@ const AddAToy = () => {
                   className=" border border-gray-100 text-gray-700 sm:text-sm rounded-lg focus:ring-primary-400 focus:border-primary-400 block w-full p-2.5"
                   rows="4"
                   cols="40"
+                  name="description"
                 ></textarea>
               </div>
-
               <button
                 type="submit"
-                className="w-full text-white bg-yellow-500 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center "
+                className="w-full btn focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center "
               >
-                Login
+                Add Toy
               </button>
             </form>
           </div>
